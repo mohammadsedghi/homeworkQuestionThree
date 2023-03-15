ebe6d4cd7ed617c7e0595cc3bed62e68bf449390
mohammad <mohammadsedghi1993@gmail.com>
2023-03-15 12:55:46 +0330

    teacher,students,course is instanctiate in UniversityApplication

--- UniversityApplication.java
+++ UniversityApplication.java
@@ -5,6 +5,9 @@ public class UniversityApplication {
 Teacher teacher=new Teacher("ali","yarmohammadi",622);
 Course course=new Course("math",622,1);
 Student student=new Student("reza","mohammadi",1);
+course.addStudent(student);
+course.addTeacher(teacher);
 course.listStudent(1);
+
     }
 }
