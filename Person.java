869d7e914c98db8832d0f3324481c7488412dd73
mohammad <mohammadsedghi1993@gmail.com>
2023-03-15 11:14:04 +0330

    constructor,getter,setter creatd in person

--- Person.java
+++ Person.java
@@ -1,7 +1,27 @@
 package questionthree;
 
 public class Person {
-    String name;
-    String lastname;
-    
+   private String name;
+    private  String lastname;
+
+    public Person(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
