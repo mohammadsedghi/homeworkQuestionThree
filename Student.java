38198707a789bf8ca028ac8a9aa6f2557168c9cb
mohammad <mohammadsedghi1993@gmail.com>
2023-03-16 01:08:55 +0330

    comment written in teacher and student class

--- Student.java
+++ Student.java
@@ -1,18 +1,19 @@
 package questionthree;
 
 public class Student extends Person {
+    //variable and properties
     private String major;
     private double score;
-
+//getter for score
     public double getScore() {
         return score;
     }
-
+//setter for score
     public void setScore(double score) {
         this.score = score;
     }
 
-
+//constructor
     public Student(String name, String lastname, int id,String major) {
         super(name, lastname, id);
         this.major=major;
--- Teacher.java
+++ Teacher.java
@@ -1,6 +1,7 @@
 package questionthree;
 
 public class Teacher extends Person{
+    //constructor
     public Teacher(String name, String lastname,int id) {
         super(name, lastname,id);
 
